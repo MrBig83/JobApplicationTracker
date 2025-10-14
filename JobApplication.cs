d1e5785eae7714868089eaad42cee6ae43f9d5fa
MrBig83 <mjohansson176@gmail.com>
2025-10-14 14:09:35 +0200

    Lagt till JobApplication och JobManager som klasser. JM innehåller ingen logik, bara placeholders.

---
+++ JobApplication.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationTracker
+{
+    public class JobApplication
+    {
+        public string CompanyName { get; set; }
+        public string PositionTitle { get; set; }
+        public enum Status { Applied, Interview, Offer, Rejected }
+        public Status CurrentState { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public DateTime ResponseDate { get; set; }
+        public int SalaryExpectation { get; set; }
+
+        public JobApplication(string companyName, string positionTitle, Enum Status, DateTime applicationDate, DateTime responseDate, int salaryExpectation)
+        {
+            CompanyName = companyName;
+            PositionTitle = positionTitle;
+            CurrentState = JobApplication.Status.Applied;
+            ApplicationDate = applicationDate;
+            ResponseDate = responseDate;
+            SalaryExpectation = salaryExpectation;
+        }
+
+        public int GetDaysSinceApplied()
+        {
+            TimeSpan diff = DateTime.Now - ApplicationDate;
+            return diff.Days;
+        }
+
+        public void GetSummary()
+        {
+            Console.WriteLine($"{PositionTitle} på {CompanyName}. Ansökte: {ApplicationDate} (för {GetDaysSinceApplied} dagar sedan). Status: {CurrentState}. ");
+        }
+    }
+}
