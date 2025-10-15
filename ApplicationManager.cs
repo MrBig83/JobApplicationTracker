0771cf5003fa51226e16b90eb80feb59a9100d2d
MrBig83 <mjohansson176@gmail.com>
2025-10-15 10:03:07 +0200

    Updated filestructure and had an issue with git. Lets see if this solves it.

--- ApplicationManager.cs
+++ ApplicationManager.cs
@@ -21,7 +21,7 @@ namespace JobApplicationTracker
             string positionTitle = Console.ReadLine();
             Console.WriteLine("Ange vilket löneanspråk du har angett:");
             int salaryExpectation = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
+            Console.WriteLine(); 
             Console.WriteLine("Fyller i resternade värden..."); //Progressbar på 1.5 sec
 
             applications.Add(new JobApplication(companyName, positionTitle, JobApplication.Status.Applied, DateTime.Now, null, salaryExpectation));
